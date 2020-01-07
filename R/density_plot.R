1697b027b2b595790067aef844a24e2a3bcba753
Ines HC <ines.hnrqs@gmail.com>
2020-01-07 14:24:45 +0000

    Export only external functions.

--- R/density_plot.R
+++ R/density_plot.R
@@ -1,5 +1,12 @@
+#' Draw a desntit plot for a numeric sample
+#' returns the plot using ggplot2
+#' @param v numeric vector
+#'
 #' @importFrom ggplot2 ggplot aes geom_density
 #' @importFrom magrittr %>%
+#' @export
+#' @examples
+#' density_plot(rnorm(50))
 density_plot<-function(v){
   stopifnot(is.numeric(v))
   data.frame(values = v) %>%
